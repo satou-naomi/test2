2c6e9a2ed0d42f026cb67702a75fc734e389be51
satou-naomi <146058532+satou-naomi@users.noreply.github.com>
2023-09-26 06:30:16 +0000

    aaa

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ st.set_page_config(page_title="タイトル", layout="wide")
 st.title('Streamlitのサンプルアプリ')
 
 # テキスト入力ボックスを作成し、ユーザーからの入力を受け取る
-user_input = st.text_input('あなたの名前を入力してください')
+user_input = st.text_input('aaあなたの名前を入力してください')
 
 # ボタンを作成し、クリックされたらメッセージを表示
 if st.button('挨拶する'):
